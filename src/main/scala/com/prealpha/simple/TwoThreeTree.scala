69df0673792a5427f8f52dd698865383844327e0
Meyer Kizner <meyer@pre-alpha.com>
2015-01-02 06:56:44 -0500

    Separate leaf and node classes, still no remove

--- src/main/scala/com/prealpha/simple/TwoThreeTree.scala
+++ src/main/scala/com/prealpha/simple/TwoThreeTree.scala
@@ -7,7 +7,7 @@ sealed abstract class TwoThreeTree[T: Ordering] extends SimpleSet[T] {
 
   override def add(elem: T): TwoThreeTree.NonEmpty[T] = doAdd(elem) match {
     case Left((leftChild, value, rightChild)) =>
-      TwoThreeTree.TwoNode(value, Some((leftChild, rightChild)))
+      TwoThreeTree.TwoNode(leftChild, value, rightChild)
     case Right(tree) =>
       tree
   }
@@ -17,7 +17,7 @@ object TwoThreeTree {
   import scala.math.Ordering.Implicits._
 
   private case class Empty[T: Ordering]() extends TwoThreeTree[T] {
-    override protected def doAdd(elem: T): Either[SplitNode, NonEmpty[T]] = Right(TwoNode(elem, None))
+    override protected def doAdd(elem: T): Either[SplitNode, NonEmpty[T]] = Right(TwoLeaf(elem))
 
     override def remove(elem: T): SimpleSet[T] = this
 
@@ -28,30 +28,45 @@ object TwoThreeTree {
 
   protected sealed abstract class NonEmpty[T: Ordering] extends TwoThreeTree[T]
 
+  private case class TwoLeaf[T: Ordering](private val value: T) extends NonEmpty[T] {
+    override protected def doAdd(elem: T): Either[SplitNode, NonEmpty[T]] = {
+      if (elem < value) {
+        Right(ThreeLeaf(elem, value))
+      } else if (elem > value) {
+        Right(ThreeLeaf(value, elem))
+      } else {
+        Right(this)
+      }
+    }
+
+    override def contains(elem: T): Boolean = {
+      elem == value
+    }
+
+    override def fold[U](base: U)(combine: (U, T) => U): U = combine(base, value)
+  }
+
   private case class TwoNode[T: Ordering](
+      private val left: NonEmpty[T],
       private val value: T,
-      private val children: Option[(NonEmpty[T], NonEmpty[T])])
+      private val right: NonEmpty[T])
     extends NonEmpty[T] {
 
     override protected def doAdd(elem: T): Either[SplitNode, NonEmpty[T]] = {
       if (elem < value) {
-        children map {
-          case (left, right) => left.doAdd(elem) match {
-            case Left((leftChild, leftValue, middleChild)) =>
-              Right(ThreeNode(leftValue, value, Some((leftChild, middleChild, right))))
-            case Right(leftChild) =>
-              Right(TwoNode(value, Some((leftChild, right))))
-          }
-        } getOrElse Right(ThreeNode(elem, value, None))
+        left.doAdd(elem) match {
+          case Left((leftChild, leftValue, middleChild)) =>
+            Right(ThreeNode(leftChild, leftValue, middleChild, value, right))
+          case Right(leftChild) =>
+            Right(TwoNode(leftChild, value, right))
+        }
       } else if (elem > value) {
-        children map {
-          case (left, right) => right.doAdd(elem) match {
-            case Left((middleChild, rightValue, rightChild)) =>
-              Right(ThreeNode(value, rightValue, Some((left, middleChild, rightChild))))
-            case Right(rightChild) =>
-              Right(TwoNode(value, Some((left, rightChild))))
-          }
-        } getOrElse Right(ThreeNode(value, elem, None))
+        right.doAdd(elem) match {
+          case Left((middleChild, rightValue, rightChild)) =>
+            Right(ThreeNode(left, value, middleChild, rightValue, rightChild))
+          case Right(rightChild) =>
+            Right(TwoNode(left, value, rightChild))
+        }
       } else {
         Right(this)
       }
@@ -59,73 +74,86 @@ object TwoThreeTree {
 
     override def contains(elem: T): Boolean = {
       if (elem < value) {
-        children exists { _._1.contains(elem) }
+        left.contains(elem)
       } else if (elem > value) {
-        children exists { _._2.contains(elem) }
+        right.contains(elem)
       } else {
         true
       }
     }
 
     override def fold[U](base: U)(combine: (U, T) => U): U = {
-      children map {
-        case (left, right) =>
-          right.fold(combine(left.fold(base)(combine), value))(combine)
-      } getOrElse combine(base, value)
+      right.fold(combine(left.fold(base)(combine), value))(combine)
+    }
+  }
+
+  private case class ThreeLeaf[T: Ordering](
+      private val leftValue: T,
+      private val rightValue: T)
+    extends NonEmpty[T] {
+
+    override protected def doAdd(elem: T): Either[SplitNode, NonEmpty[T]] = {
+      if (elem < leftValue) {
+        val leftChild = TwoLeaf(elem)
+        val rightChild = TwoLeaf(rightValue)
+        Left((leftChild, leftValue, rightChild))
+      } else if (elem > rightValue) {
+        val leftChild = TwoLeaf(leftValue)
+        val rightChild = TwoLeaf(elem)
+        Left((leftChild, rightValue, rightChild))
+      } else if (elem != leftValue && elem != rightValue) {
+        val leftChild = TwoLeaf(leftValue)
+        val rightChild = TwoLeaf(rightValue)
+        Left((leftChild, elem, rightChild))
+      } else {
+        Right(this)
+      }
+    }
+
+    override def contains(elem: T): Boolean = {
+      elem == leftValue || elem == rightValue
+    }
+
+    override def fold[U](base: U)(combine: (U, T) => U): U = {
+      combine(combine(base, leftValue), rightValue)
     }
   }
 
   private case class ThreeNode[T: Ordering](
+      private val left: NonEmpty[T],
       private val leftValue: T,
+      private val middle: NonEmpty[T],
       private val rightValue: T,
-      private val children: Option[(NonEmpty[T], NonEmpty[T], NonEmpty[T])])
+      private val right: NonEmpty[T])
     extends NonEmpty[T] {
 
     override protected def doAdd(elem: T): Either[SplitNode, NonEmpty[T]] = {
       if (elem < leftValue) {
-        children map {
-          case (left, middle, right) => left.doAdd(elem) match {
-            case Left((farLeft, value, leftMiddle)) =>
-              val leftChild = TwoNode(value, Some((farLeft, leftMiddle)))
-              val rightChild = TwoNode(rightValue, Some((middle, right)))
-              Left((leftChild, leftValue, rightChild))
-            case Right(leftChild) =>
-              Right(ThreeNode(leftValue, rightValue, Some((leftChild, middle, right))))
-          }
-        } getOrElse {
-          val leftChild = TwoNode(elem, None)
-          val rightChild = TwoNode(rightValue, None)
-          Left((leftChild, leftValue, rightChild))
+        left.doAdd(elem) match {
+          case Left((farLeft, value, leftMiddle)) =>
+            val leftChild = TwoNode(farLeft, value, leftMiddle)
+            val rightChild = TwoNode(middle, rightValue, right)
+            Left((leftChild, leftValue, rightChild))
+          case Right(leftChild) =>
+            Right(ThreeNode(leftChild, leftValue, middle, rightValue, right))
         }
       } else if (elem > rightValue) {
-        children map {
-          case (left, middle, right) => right.doAdd(elem) match {
-            case Left((rightMiddle, value, farRight)) =>
-              val leftChild = TwoNode(leftValue, Some((left, middle)))
-              val rightChild = TwoNode(value, Some((rightMiddle, farRight)))
-              Left((leftChild, rightValue, rightChild))
-            case Right(rightChild) =>
-              Right(ThreeNode(leftValue, rightValue, Some((left, middle, rightChild))))
-          }
-        } getOrElse {
-          val leftChild = TwoNode(leftValue, None)
-          val rightChild = TwoNode(elem, None)
-          Left((leftChild, rightValue, rightChild))
+        right.doAdd(elem) match {
+          case Left((rightMiddle, value, farRight)) =>
+            val leftChild = TwoNode(left, leftValue, middle)
+            val rightChild = TwoNode(rightMiddle, value, farRight)
+            Left((leftChild, rightValue, rightChild))
+          case Right(rightChild) =>
+            Right(ThreeNode(left, leftValue, middle, rightValue, rightChild))
         }
       } else if (elem != leftValue && elem != rightValue) {
-        children map {
-          case (left, middle, right) => middle.doAdd(elem) match {
-            case Left((leftMiddle, value, rightMiddle)) =>
-              val leftChild = TwoNode(leftValue, Some((left, leftMiddle)))
-              val rightChild = TwoNode(rightValue, Some((rightMiddle, right)))
-              Left((leftChild, value, rightChild))
-            case Right(middleChild) =>
-              Right(ThreeNode(leftValue, rightValue, Some((left, middleChild, right))))
-          }
-        } getOrElse {
-          val leftChild = TwoNode(leftValue, None)
-          val rightChild = TwoNode(rightValue, None)
-          Left((leftChild, elem, rightChild))
+        middle.doAdd(elem) match {
+          case Left((leftMiddle, value, rightMiddle)) =>
+            val leftChild = TwoNode(left, leftValue, leftMiddle)
+            val rightChild = TwoNode(rightMiddle, rightValue, right)
+            Left((leftChild, value, rightChild))
+          case Right(middleChild) =>
+            Right(ThreeNode(left, leftValue, middleChild, rightValue, right))
         }
       } else {
         Right(this)
@@ -134,21 +162,18 @@ object TwoThreeTree {
 
     override def contains(elem: T): Boolean = {
       if (elem < leftValue) {
-        children exists { _._1.contains(elem) }
+        left.contains(elem)
       } else if (elem > rightValue) {
-        children exists { _._3.contains(elem) }
+        right.contains(elem)
       } else if (elem != leftValue && elem != rightValue) {
-        children exists { _._2.contains(elem) }
+        middle.contains(elem)
       } else {
         true
       }
     }
 
     override def fold[U](base: U)(combine: (U, T) => U): U = {
-      children map {
-        case (left, middle, right) =>
-          right.fold(combine(middle.fold(combine(left.fold(base)(combine), leftValue))(combine), rightValue))(combine)
-      } getOrElse combine(combine(base, leftValue), rightValue)
+      right.fold(combine(middle.fold(combine(left.fold(base)(combine), leftValue))(combine), rightValue))(combine)
     }
   }
 }
